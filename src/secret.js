6006933359b56bcd3062d647ba6d5a85fb7b61a3
Xander Siruno-Nebel <73612166+xandervsn@users.noreply.github.com>
2025-01-15 23:00:28 -0800

    Update secret.js

--- src/secret.js
+++ src/secret.js
@@ -1,5 +1,5 @@
-const lastKey = "ac172dfb9e833c488a822d2249b69fc6"
-const deezerKey = "8e3e2245cfmshce1e348d643108ap1931f5jsn33798b69504a"
+const lastKey = "KEY"
+const deezerKey = "KEY"
 
 function getLastKey(){
     return lastKey
